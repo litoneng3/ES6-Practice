46c9e41130c3c3c4c0e95facdc69e217e790a32d
litoneng3 <47786100+litoneng3@users.noreply.github.com>
2021-02-01 22:43:27 +0600

    this is ES6

---
+++ template.js
@@ -0,0 +1,16 @@
+const firstName= "Liton";
+const lastName = "Hossain";
+const fullName = firstName +" "+ lastName + " is a good boy.";
+const fullName2 = `${firstName} ${lastName} is a good human.`;
+console.log(fullName2);
+console.log(fullName);
+
+const letter = "I am a student of RUET.\n"
+            +"This is my first web development";
+console.log(letter);
+
+const letter2 = `This is Liton Hossain.
+I am nothing.
+I am a simple human.`;
+
+console.log(letter2);
